e9305bdf4a4615788c7b136ceafd138421829905
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2018-07-03 19:22:19 +0200

    Extract DataFetcher

---
+++ data_fetcher.rb
@@ -0,0 +1,57 @@
+require 'net/http'
+require 'nokogiri'
+require 'selenium-webdriver'
+
+class DataFetcher
+  WINDOW_SIZE = 10
+  ClanWarParticipants = Struct.new(:clan_war_ago, :day_1, :day_2)
+
+  def update_clan_info
+    options = Selenium::WebDriver::Firefox::Options.new
+    options.add_argument('--headless')
+    driver = Selenium::WebDriver.for :firefox, options: options
+
+    driver.navigate.to 'https://statsroyale.com/clan/98YGJ2YJ'
+    # driver.navigate.to "https://statsroyale.com/clan/LU2VCPU"
+    element = driver.find_element(css: '.clan__refreshButton')
+    driver.execute_script("arguments[0].click()", element)
+    driver.quit
+  end
+
+  def fetch_clan_members
+    response = Net::HTTP.get(URI('https://statsroyale.com/clan/LU2VCPU'))
+    Nokogiri::HTML(response).css('.clan__table .clan__rowContainer').map do |row|
+      row.css('.clan__row:nth-child(2)').text.strip
+    end
+  end
+
+  def fetch_clan_war_participants
+    response = Net::HTTP.get(URI('https://statsroyale.com/clan/LU2VCPU/war/history'))
+
+    extract_player_name = -> (el) { el.css('.clanParticipants__row:nth-child(2)').text.strip }
+
+    Nokogiri::HTML(response).css('.clanWarHistory__modal').take(WINDOW_SIZE).map.with_index do |clan_war, index|
+      day_1 = clan_war.css('.clanParticipants__rowContainer')
+        .map(&extract_player_name)
+
+      day_2 = clan_war
+        .css('.clanParticipants__rowContainer')
+        .select{ |el| el.css('.clanParticipants__row:nth-child(3)').text.strip != '0'}
+        .map(&extract_player_name)
+
+      ClanWarParticipants.new(WINDOW_SIZE - index, day_1, day_2)
+    end
+  end
+
+  def mock_clan_members
+    ['adi', 'bob', 'ciele']
+  end
+
+  def mock_clan_war_participants
+    [
+      ClanWarParticipants.new(1, ['adi', 'bob'], ['bob']),
+      ClanWarParticipants.new(2, ['adi', 'bob'], ['adi', 'bob']),
+      ClanWarParticipants.new(3, ['ciele', 'bob'], ['ciele']),
+    ]
+  end
+end
--- script.rb
+++ script.rb
@@ -1,51 +1,11 @@
-require 'net/http'
-require 'nokogiri'
 require_relative 'table_creator'
+require_relative 'data_fetcher'
 
-WINDOW_SIZE = 10
-ClanWarParticipants = Struct.new(:clan_war_ago, :day_1, :day_2)
+fetcher = DataFetcher.new
+fetcher.update_clan_info
 
-def fetch_clan_members
-  response = Net::HTTP.get(URI('https://statsroyale.com/clan/LU2VCPU'))
-  Nokogiri::HTML(response).css('.clan__table .clan__rowContainer').map do |row|
-    row.css('.clan__row:nth-child(2)').text.strip
-  end
-end
-
-def fetch_clan_war_participants
-  response = Net::HTTP.get(URI('https://statsroyale.com/clan/LU2VCPU/war/history'))
-
-  extract_player_name = -> (el) { el.css('.clanParticipants__row:nth-child(2)').text.strip }
-
-  Nokogiri::HTML(response).css('.clanWarHistory__modal').take(WINDOW_SIZE).map.with_index do |clan_war, index|
-    day_1 = clan_war.css('.clanParticipants__rowContainer')
-      .map(&extract_player_name)
-
-    day_2 = clan_war
-      .css('.clanParticipants__rowContainer')
-      .select{ |el| el.css('.clanParticipants__row:nth-child(3)').text.strip != '0'}
-      .map(&extract_player_name)
-
-    ClanWarParticipants.new(WINDOW_SIZE - index, day_1, day_2)
-  end
-end
-
-def get_lazy_members(members, participants)
-  members - participants.map(&:day_2).flatten.uniq
-end
-
-members = fetch_clan_members
-participants = fetch_clan_war_participants
-
-lazy_shits = get_lazy_members(members, participants)
-puts lazy_shits
-
-# members = ['adi', 'bob', 'ciele']
-# participants = [
-#   ClanWarParticipants.new(1, ['adi', 'bob'], ['bob']),
-#   ClanWarParticipants.new(2, ['adi', 'bob'], ['adi', 'bob']),
-#   ClanWarParticipants.new(3, ['ciele', 'bob'], ['ciele']),
-# ]
+members = fetcher.fetch_clan_members
+participants = fetcher.fetch_clan_war_participants
 
 table_creator = TableCreator.new(members, participants)
 
